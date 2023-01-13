7115275262cd54ec4b990dd601c7386cd5eb4c5b
phuong.ha <buithiphuongha2003@gmail.com>
2023-01-13 16:59:03 +0700

    first commit

---
+++ client/src/features/Catalog/ProductCard.tsx
@@ -0,0 +1,52 @@
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  Typography,
+} from "@mui/material";
+import { Link } from "react-router-dom";
+import { product } from "../../models/product";
+
+interface Props {
+  product: product;
+}
+
+export default function ProductCard({ product }: Props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardHeader
+        avatar={
+          <Avatar sx={{ bgcolor: "primary.main" }}>
+            {product.name.charAt(0).toUpperCase()}
+          </Avatar>
+        }
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", color: "secondary.main" },
+        }}
+      />
+
+      <CardMedia
+        sx={{ height: 140 , backgroundSize: "contain" , bgcolor: 'primary.main'}}
+        image="http://picksum.photos/200"
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div" color="secondary">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add To Cart</Button>
+        <Button component={Link} to={`/Catalog/${product.id}`}  size="small">View</Button>
+      </CardActions>
+    </Card>
+  );
+}
